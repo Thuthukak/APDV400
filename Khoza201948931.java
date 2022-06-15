41f28d70d1aeeb858e08801e1849eb36d3892268
Thuthukak <88458223+Thuthukak@users.noreply.github.com>
2022-06-15 11:32:52 +0200

    Add files via upload

---
+++ Khoza201948931.java
@@ -0,0 +1,25 @@
+
+
+package koza201948931;
+
+/**
+ *
+ * @author Registrations2
+ */
+public class Khoza201948931 {
+
+    /**
+     * @param args the command line arguments
+     * @param Z
+     */
+    public static void main(String[] args, String Z) {
+        // TODO code application logic here
+        int x = 5;
+			int y = 2;
+			int z = x * y;
+                
+
+		System.out.println("x=5 * y=2 is equal to "+Z);
+    }
+    
+}
